e04061c10651fffecbbb84c5488cf55325a84b97
hansteminnovations <hansteminnovations@gmail.com>
2026-03-14 12:39:04 +0530

    Python module 1

---
+++ Module 1/Lesson 1.py
@@ -0,0 +1,8 @@
+# Printing a greeting message
+print("Welcome to the world of coding!")
+
+print("My name is Francis Santos.")
+
+print("I love playing Football.")
+
+print("The sum of 49 + 21 is:", 49 + 21)
