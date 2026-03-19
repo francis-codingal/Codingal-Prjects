9732682b350d3eedc67ea252b748a3c3ea732a95
hansteminnovations <hansteminnovations@gmail.com>
2026-03-19 12:29:39 +0530

    acp4

--- Module 1/Lesson 4 ACP.py
+++ Module 1/Lesson 4 ACP.py
@@ -1,8 +1,4 @@
-# 1. Get input from the user
 num = float(input("Enter a number: "))
-
-# 2. Calculate square root using the ** operator
-# Raising a number to the power of 0.5 is the same as √
 result = num ** 0.5
 # 3. Display the result
 print("The square root is:", result)
