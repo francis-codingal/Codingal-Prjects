3a1c01fe929704286e075053fa43dea2a435de78
hansteminnovations <hansteminnovations@gmail.com>
2026-03-19 12:21:33 +0530

    commit

---
+++ Module 1/Lesson 3 ACP.py
@@ -0,0 +1,20 @@
+# 1. Storing a string in a variable
+message = "Welcome to Python Coding!"
+
+# 2. Concatenation
+greeting = "Hello! " + message
+print("Joined String:", greeting)
+
+# 3. Repetition 
+stars = "*" * 10
+print("Repeated String:", stars)
+
+# 4. Upper and Lower Case
+print("Upper Case:", message.upper())
+print("Lower Case:", message.lower())
+
+# 5. Length of a string
+print("Number of characters:", len(message))
+
+# 6. Slicing
+print("First word:", message[0:7])
