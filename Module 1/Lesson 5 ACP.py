c57d95417ac79e52bfe112a617f9937aaa0e9f4c
hansteminnovations <hansteminnovations@gmail.com>
2026-03-19 12:42:13 +0530

    acp5

---
+++ Module 1/Lesson 5 ACP.py
@@ -0,0 +1,10 @@
+temp = float(input("What is the temperature outside (in °C)? "))
+
+if temp >= 35:
+    print("It's a heatwave! Stay inside and have some ice cream.")
+elif temp >= 25:
+    print("Perfect summer weather! It's a great time for a swim.")
+elif temp >= 15:
+    print("It's a pleasant day. Maybe go for a walk in the park?")
+else:
+    print("It doesn't feel much like summer. Better grab a light jacket!")
