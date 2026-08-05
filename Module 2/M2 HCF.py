128372693562f58adadba8ffbac1fa0262d0ed3f
Francis Santos <francissantos.codingal@gmail.com>
2026-08-05 15:48:20 +0530

    Add HCF calculation script in M2 HCF.py

---
+++ Module 2/M2 HCF.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+a, b = num1, num2
+while b > 0:
+    a, b = b, a % b
+
+print(f"The HCF of {num1} and {num2} is {a}")
