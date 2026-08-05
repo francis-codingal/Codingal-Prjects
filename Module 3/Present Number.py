515306c23b39598e16c10b801e3ed8bea33d9b83
Francis Santos <francissantos.codingal@gmail.com>
2026-08-05 16:26:42 +0530

    Present Num

---
+++ Module 3/Present Number.py
@@ -0,0 +1,7 @@
+def print_numbers(limit):
+    for num in range(1, limit + 1):
+        present_number = num
+        print("Present number:", present_number)
+
+max_count = 5
+print_numbers(max_count)
